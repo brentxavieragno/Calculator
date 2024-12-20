02aed46e721a0c165b792208cdd59d605cd9fada
Brent Xavier Agno <agnobrentxavier462@gmail.com>
2024-12-20 13:38:01 +0800

    (calculator): implement all button functionalities
    - Added functionality for numeric buttons (0-9) and operators (+, -, *, /, %).
    - Implemented "=" for result calculation.
    - Prevented invalid inputs, like multiple dots or divide by zero.
    - Enhanced display formatting to remove unnecessary decimals (e.g., 2.0 -> 2).
    - Included "C" button to clear the display and reset calculations.
    - Changes display Background
    
    This completes the core functionality of the calculator.

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalculatorGUI extends JFrame {
 
@@ -9,19 +10,20 @@ public class CalculatorGUI extends JFrame {
     private JTextField displayText;
     private JButton[] button;
 
+    private double num1, num2, result;
+    private char operator;
+
     public CalculatorGUI() {
         setTitle(config.WINDOW_NAME);
         setResizable(false);
-        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         addUIComponents();
-
-
     }
 
     public void initialize() {
         setVisible(true);
         pack();
+        setLocationRelativeTo(null);
     }
 
     public void addUIComponents() {
@@ -33,7 +35,7 @@ public class CalculatorGUI extends JFrame {
         displayPanel = new JPanel();
         displayPanel.setPreferredSize(new Dimension(config.DISPLAY_SIZE[0], config.DISPLAY_SIZE[1]));
         displayPanel.setLayout(layout);
-        displayPanel.setBackground(Color.WHITE);
+        displayPanel.setBackground(Color.BLACK);
 
         displayText = new JTextField(config.TEXT_COLUMN);
         createTextField(displayText);
@@ -59,52 +61,145 @@ public class CalculatorGUI extends JFrame {
     public void createButtonFieldComponents(){
         buttonPanel = new JPanel();
         buttonPanel.setPreferredSize(new Dimension(config.BUTTON_PANELSIZE[0], config.BUTTON_PANELSIZE[1]));
-        buttonPanel.setBounds(0, 150, 500, 500);
+        buttonPanel.setBounds(0, 150, config.BUTTONFIELD_SIZE[0], config.BUTTONFIELD_SIZE[0]);
         buttonPanel.setBackground(Color.WHITE);
 
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        buttonPanel.setBorder(config.BUTTON_BORDER);
 
         buttonPanel.setLayout(new GridLayout(config.BUTTON_ROW, config.BUTTON_COLUMN, config.BUTTON_HGAP, config.BUTTON_VGAP));
         button = new JButton[config.BTN_NUMBER];
-        createButton(button, buttonPanel, config.BTN_NUMBER);
+        button = createButton(button, buttonPanel, config.BTN_NUMBER);
+
+        for (int i = 0; i < config.BTN_NUMBER; i++) {
+            keyPressedActions(button[i]);
+        }
 
         add(buttonPanel, BorderLayout.SOUTH);
     }
 
 
     public JButton[] createButton(JButton[] btn, JPanel btnPannel, int btnCount) {
-        btn = new JButton[btnCount];
         for (int i = 0; i < btnCount; i++) {
-            button[i] = new JButton(config.BTN_TEXT[i]);
+            btn[i] = new JButton(config.BTN_TEXT[i]);
             btnPannel.add(button[i]);
 
-            button[i].setBackground(config.BUTTON_NUMBER_COLOR);
-            button[i].setFont(new Font("Lucida Console", Font.BOLD, 30));
-            button[i].setForeground(Color.WHITE);
-            button[i].setFocusable(false);
-            button[i].setBorder(null);
-
+            btn[i].setBackground(config.BUTTON_NUMBER_COLOR);
+            btn[i].setFont(config.BUTTON_FONT);
+            btn[i].setForeground(Color.WHITE);
+            btn[i].setFocusable(false);
+            btn[i].setBorder(null);
 
             if (i <= 2) {
-                button[i].setBackground(new Color(128,128,128,255));
+                btn[i].setBackground(config.SPECIALBUTTON_COLOR);
             } else if (i == 3 || i == 7 || i == 11 || i == 15) {
-                button[i].setBackground(new Color(234,150,57,255));
-                button[i].setForeground(Color.BLACK);
+                btn[i].setBackground(config.ARITHMETIC_COLOR);
+                btn[i].setForeground(Color.BLACK);
             } else {
-                button[i].setBackground(new Color(229,229,229,255));
-                button[i].setForeground(Color.BLACK);
+                btn[i].setBackground(config.NUMBER_COLOR);
+                btn[i].setForeground(Color.BLACK);
             }
-
         }
-
         button[18].setPreferredSize(new Dimension(100, 100));
-
-
         return btn;
     }
 
+    public JButton keyPressedActions(JButton button) {
 
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String keyCommands = e.getActionCommand();
+                // Numerical Values
+                if ("0123456789".contains(keyCommands)) {
+                    if (displayText.getText().equals("0")) {
+                        displayText.setText(keyCommands);
+                    } else if (displayText.getText().equals("Error")) {
+                        displayText.setText(keyCommands);
+                    } else {
+                        displayText.setText(displayText.getText() + keyCommands);
+                    }
+                }
 
+                // Clear key
+                if ("C".contains(keyCommands)) {
+                    displayText.setText("0");
+                    num1 = 0;
+                    num2 = 0;
+                    operator = '\0';
+                }
+
+                // Backspace key
+                if ("←".contains(keyCommands)) {
+                    displayText.setText(displayText.getText().substring(0, displayText.getText().length() -1));
+                    if (displayText.getText().isEmpty()) {
+                        displayText.setText("0");
+                    }
+                }
+
+                // Decimal Key
+                if (".".contains(keyCommands)) {
+                    if (!displayText.getText().contains(".")) {
+                        displayText.setText(displayText.getText() + ".");
+                    }
+                }
+
+                // Arithmetic Operators
+                if ("+-%÷×".contains(keyCommands)) {
+                    if (!displayText.getText().equals("0")) {
+                        num1 = Double.parseDouble(displayText.getText());
+                        operator = keyCommands.charAt(0);
+                        displayText.setText("0");
+                    }
+                }
+
+                // Equal key
+                if ("=".contains(keyCommands)) {
+                    if (!displayText.getText().isEmpty()) {
+                        num2 = Double.parseDouble(displayText.getText());
+
+                        if (operator != '\0') {
+                            switch (operator) {
+                                case '+': result = num1 + num2; break;
+                                case '-': result = num1 - num2; break;
+                                case '×': result = num1 * num2;break;
+                                case '%':
+                                    if (num2 != 0) result = num1 % num2;
+                                    else {
+                                        displayText.setText("Error");
+                                        return;
+                                    }
+                                    break;
+                                case '÷':
+                                    if (num2 != 0) result = num1 / num2;
+                                    else {
+                                        displayText.setText("Error");
+                                        return;
+                                    }
+                                    break;
+                            }
+                            displayText.setText(String.valueOf(result));
+
+                            if (result == (int) result) {
+                                displayText.setText(String.valueOf((int)result));
+                            } else {
+                                displayText.setText(String.valueOf(result));
+                            }
+                            operator = '\0';
+                            num1 = result;
+                        } else {
+                            if (num2 == (int) num2) {
+                                displayText.setText(String.valueOf((int)num2));
+                            } else {
+                                displayText.setText(String.valueOf(num2));
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        return button;
+    }
 
 
 
--- src/config.java
+++ src/config.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class config {
@@ -8,18 +10,26 @@ public class config {
 
 
     public static final int TEXT_COLUMN = 6;
-    public static final int TEXT_FONTSIZE = 80;
-    public static final Color TEXT_COLOR = Color.BLACK;
-    public static final Font TEXT_FONT = new Font("Lucida Console", Font.PLAIN, TEXT_FONTSIZE);
+    public static final int TEXT_FONTSIZE = 60;
+    public static final Color TEXT_COLOR = Color.WHITE;
+    public static final Font TEXT_FONT = new Font("Consolas", Font.PLAIN, TEXT_FONTSIZE);
 
+    // BUTTON CONFIGS
     public static final int[] BUTTON_PANELSIZE = {400, 500};
     public static final int BUTTON_ROW = 5;
     public static final int BUTTON_COLUMN = 4;
+    public static final Font BUTTON_FONT = new Font("Lucida Console", Font.BOLD, 30);
 
+    public static final int[] BUTTONFIELD_SIZE = {500, 500};
+    public static final Border BUTTON_BORDER = BorderFactory.createEmptyBorder(20, 20, 20, 20);
+
+    // BUTTON GAP
     public static final int BUTTON_HGAP = 20;
     public static final int BUTTON_VGAP = 20;
 
+    // NUMBER OF BUTTONS
     public static final int BTN_NUMBER = 19;
+    // BUTTON LABELS
     public static final String[] BTN_TEXT = {
             "←", "C", "%", "÷",
             "7", "8", "9","×",
@@ -27,8 +37,12 @@ public class config {
             "1", "2", "3", "+",
             "0", ".", "="
     };
-
+    // BUTTON COLOR
     public static final Color BUTTON_NUMBER_COLOR = new Color(51,51,51,255);
-
-
+    // SPECIAL BUTTON COLORS
+    public static final Color SPECIALBUTTON_COLOR = new Color(128,128,128,255);
+    // ARITHMETIC BUTTON COLOR
+    public static final Color ARITHMETIC_COLOR = new Color(234,150,57,255);
+    // NUMBER COLOR
+    public static final Color NUMBER_COLOR = new Color(229, 229, 229, 255);
 }
